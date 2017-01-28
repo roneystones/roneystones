f61c8d07b53dd17e62509f607ee054064390d95c
RONEY-AS\Roney AS <roney.lacerda@actualsales.com.br>
2017-01-27 22:14:07 -0200

    divide em src e dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp')
-	,imagemin = require('gulp-imagemin');
+	,imagemin = require('gulp-imagemin')
+	,clean = require('gulp-clean');
 
+gulp.task('copy',['clean'], function() {
+	return gulp.src('src/**/*')
+		.pipe(gulp.dest('dist'));
+});
 
-gulp.task('build-img', function() {
-	gulp.src('images/**/*')
+gulp.task('clean', function() {
+	return gulp.src('dist')
+		.pipe(clean());
+});
+
+gulp.task('build-img',['copy'], function() {
+	gulp.src('src/img/**/*')
 		.pipe(imagemin())
-		.pipe(gulp.dest('images'));
+		.pipe(gulp.dest('dist/img'));
 });
