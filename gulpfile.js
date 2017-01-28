b23df8f71d152550ba5dc4a4b407388fe3a45cb6
RONEY-AS\Roney AS <roney.lacerda@actualsales.com.br>
2017-01-28 00:01:58 -0200

    cria novas tarefas gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,37 @@
 var gulp = require('gulp')
-	,imagemin = require('gulp-imagemin')
-	,clean = require('gulp-clean');
+  ,imagemin = require('gulp-imagemin')
+  ,clean = require('gulp-clean')
+  ,concat = require('gulp-concat')
+  ,htmlReplace = require('gulp-html-replace')
+  ,uglify = require('gulp-uglify')
+  ,usemin = require('gulp-usemin')
+  ,cssmin = require('gulp-cssmin');
 
-gulp.task('copy',['clean'], function() {
-	return gulp.src('src/**/*')
-		.pipe(gulp.dest('dist'));
+gulp.task('default', ['copy'], function() {
+  gulp.start('build-img', 'usemin');
+});
+
+gulp.task('copy', ['clean'], function() {
+  return gulp.src('src/**/*')
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', function() {
-	return gulp.src('dist')
-		.pipe(clean());
+  return gulp.src('dist')
+    .pipe(clean());
 });
 
-gulp.task('build-img',['copy'], function() {
-	gulp.src('src/img/**/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('dist/img'));
+gulp.task('build-img', function() {
+  return gulp.src('dist/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function() {
+  return gulp.src('dist/**/*.html')
+    .pipe(usemin({
+      js: [uglify],
+      css: [cssmin]
+    }))
+    .pipe(gulp.dest('dist'));
 });
